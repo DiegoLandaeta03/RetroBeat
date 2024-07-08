e54a9fdd6fddcf02c167a272d2f75d089fd664c0
Diego Alejandro Landaeta Torres <diegolandaeta@meta.com>
2024-07-08 14:40:45 -0700

    [chakra][react] Re-styled profile and menu buttons to match with login button. Creates a more consistent UI

--- front-end/src/Navbar.jsx
+++ front-end/src/Navbar.jsx
@@ -1,19 +1,31 @@
-import { useNavigate } from 'react-router-dom'
-import { useState } from 'react'
-import { Box, Flex, Spacer, Heading, Text } from "@chakra-ui/react";
-import './Navbar.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+    Box,
+    Flex,
+    Spacer,
+    Heading,
+    Text,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+} from "@chakra-ui/react";
+
+import { LockIcon, SunIcon, AddIcon } from '@chakra-ui/icons';
 
 function Navbar({ username }) {
-    const navigate = useNavigate()
-    const [isDropdownVisible, setDropdownVisible] = useState(false)
+    const navigate = useNavigate();
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
 
     const handleHome = () => {
-        navigate(`/${username}`)
-    }
+        navigate(`/${username}`);
+    };
 
     const handleCreate = () => {
-        navigate(`/${username}/create`)
-    }
+        navigate(`/${username}/create`);
+    };
 
     const handleLogout = () => {
         try {
@@ -25,59 +37,112 @@ function Navbar({ username }) {
                 body: JSON.stringify({
                     token: localStorage.getItem('access_token')
                 })
-            })
+            });
 
-            localStorage.removeItem('access_token')
-            navigate('/')
+            localStorage.removeItem('access_token');
+            navigate('/');
         } catch (error) {
-            alert('Failed to logout')
+            alert('Failed to logout');
         }
-    }
-
-    const handleMouseEnter = () => {
-        setDropdownVisible(true);
-    }
-
-    const handleMouseLeave = () => {
-        setDropdownVisible(false);
-    }
+    };
 
     return (
         <Box w="70%" color="white" p={4} mx="auto">
             <Flex align="center">
-                <button className="profileIcon">
-                    <Text fontSize={'lg'}>Profile</Text>
-                </button>
-                <Spacer />
-                <Heading as='h1' size='2xl' style={{ textAlign: 'center', flexGrow: 1 }}>SoundStitch</Heading>
-                <Spacer />
-                <div
-                    className="menu"
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
+                <Button
+                    className="profileIcon"
+                    bgGradient="linear(to-r, rgba(115, 41, 123, 0.9), rgb(83, 41, 140, 0.9))"
+                    borderRadius="full"
+                    color="white"
+                    px={6}
+                    py={3}
+                    _hover={{
+                        opacity: 1,
+                        backgroundSize: 'auto',
+                        boxShadow: '0 0 20px -2px rgba(195, 111, 199, .5)',
+                        transform: 'translate3d(0, -0.5px, 0) scale(1.01)',
+                    }}
                 >
-                    <button className="dropdownButton">
-                        <Text fontSize={'lg'}>Menu</Text>
-                    </button>
-                    {isDropdownVisible && (
-                        <div className="dropdownMenu" style={{ position: 'absolute', right: 0 }}>
-                            <ul>
-                                <li onClick={handleHome}>
-                                    <Text fontSize={'md'}>Home</Text>
-                                </li>
-                                <li onClick={handleCreate}>
-                                    <Text fontSize={'md'}>Create</Text>
-                                </li>
-                                <li onClick={handleLogout}>
-                                    <Text fontSize={'md'}>Logout</Text>
-                                </li>
-                            </ul>
-                        </div>
-                    )}
-                </div>
+                    <Text fontSize="lg">Profile</Text>
+                </Button>
+
+                <Spacer />
+
+                <Heading as='h1' size='2xl' textAlign='center' flexGrow={1}>
+                    SoundStitch
+                </Heading>
+
+                <Spacer />
+
+                <Menu>
+                    <MenuButton
+                        as={Button}
+                        aria-label='Options'
+                        bgGradient="linear(to-r, rgba(115, 41, 123, 0.9), rgb(83, 41, 140, 0.9))"
+                        color={'white'}
+                        _hover={{
+                            opacity: 1,
+                            backgroundSize: 'auto',
+                            boxShadow: '0 0 20px -2px rgba(195, 111, 199, .5)',
+                            transform: 'translate3d(0, -0.5px, 0) scale(1.01)',
+                        }}>
+                        <Text fontSize="lg">Menu</Text>
+                    </MenuButton>
+
+                    <MenuList style={{ padding: 0, margin: 0 }} bgColor="#242424">
+                        <MenuItem
+                            style={{ margin: 0 }}
+                            icon={<SunIcon />}
+                            onClick={handleHome}
+                            color={'black'}
+                            _hover={{
+                                bg: 'linear-gradient(0deg, rgba(115, 41, 123, 0.9) 10%, rgba(83, 41, 140, 0.9) 100%)',
+                                color: 'white',
+                            }}
+                            _focus={{
+                                bg: 'linear-gradient(0deg, rgba(115, 41, 123, 0.9) 10%, rgba(83, 41, 140, 0.9) 100%)',
+                                color: 'white',
+                            }}
+                        >
+                            <Text fontSize="lg">Home</Text>
+                        </MenuItem>
+                        <MenuItem
+                            style={{ margin: 0 }}
+                            icon={<AddIcon />}
+                            onClick={handleCreate}
+                            color={'black'}
+                            _hover={{
+                                bg: 'linear-gradient(0deg, rgba(115, 41, 123, 0.9) 10%, rgba(83, 41, 140, 0.9) 100%)',
+                                color: 'white',
+                            }}
+                            _focus={{
+                                bg: 'linear-gradient(0deg, rgba(115, 41, 123, 0.9) 10%, rgba(83, 41, 140, 0.9) 100%)',
+                                color: 'white',
+                            }}
+                        >
+                            <Text fontSize="lg">Create Playlist</Text>
+                        </MenuItem>
+                        <MenuItem
+                            style={{ margin: 0 }}
+                            icon={<LockIcon />}
+                            onClick={handleLogout}
+                            color={'black'}
+                            _hover={{
+                                bg: 'linear-gradient(0deg, rgba(115, 41, 123, 0.9) 10%, rgba(83, 41, 140, 0.9) 100%)',
+                                color: 'white',
+                            }}
+                            _focus={{
+                                bg: 'linear-gradient(0deg, rgba(115, 41, 123, 0.9) 10%, rgba(83, 41, 140, 0.9) 100%)',
+                                color: 'white',
+                            }}
+                        >
+                            <Text fontSize="lg">Logout</Text>
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
             </Flex>
         </Box>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
